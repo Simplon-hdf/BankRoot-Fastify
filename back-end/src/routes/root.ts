a1b9fec1349fbea8200828c2ded3dac2b829ef96
Hamid Berkaine <hamiddev78@gmail.com>
2022-03-09 10:20:51 +0100

    fix path route in root.ts

--- back-end/src/routes/root.ts
+++ back-end/src/routes/root.ts
@@ -2,11 +2,11 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply:any) {
-     return reply.view('views/home', { title: 'Home', pageTitle: 'Home', message: 'Client list : ' }) 
+     return reply.view('/src/views/home', { title: 'Home', pageTitle: 'Home', message: 'Client list : ' }) 
   })
 
   fastify.get('/client', async function (request, reply:any) {
-    return reply.view('views/client', { title: 'Client', pageTitle: 'Client' }) 
+    return reply.view('/src/views/client', { title: 'Client', pageTitle: 'Client' }) 
  })
 }
 
