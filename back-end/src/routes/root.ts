c2bc44b67ceb727380ac70ac47619423ad97125b
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-03-10 15:16:50 +0100

    adding client page

--- back-end/src/routes/root.ts
+++ back-end/src/routes/root.ts
@@ -10,22 +10,13 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
       const listPerson = await prisma.person.findMany({
          where: { roleId: 2 }
       })
-
       reply.view('src/views/home', { listPerson: listPerson, balance: balance })
    })
 
-   // fastify.get('/client/:id', async (request, reply:any) => {
-   //    const listPerson = await prisma.person.findUnique({
-   //       where: { roleId: 2 }
-   //    })
-
-   //    reply.view('src/views/client', { listPerson: listPerson })
-   // })
-
-   // fastify.get('/client', async function (request, reply:any) {
-   //    reply.view('src/views/client', { title: 'Client', pageTitle: 'Client' }) 
-   // })
-
+   fastify.get('/client/:idPerson', async (request, reply:any) => {
+      const listPerson = await prisma.person.findMany()
+      reply.view('src/views/client', { listPerson: listPerson })
+   })
 }
 
 export default root;
