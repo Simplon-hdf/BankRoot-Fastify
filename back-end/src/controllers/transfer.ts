456e5d3766614d448b134c949014187cb9836476
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-03-14 03:47:51 +0100

    adding functionnel transfert

--- back-end/src/controllers/transfer.ts
+++ back-end/src/controllers/transfer.ts
@@ -1,61 +1,75 @@
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
-// type PrismaClient.accountSelect = { personId?: true }
-
-
-// async function transfer(from: string, to: string, amount: number) {
-//     return await prisma.$transaction(async (prisma) => {
-//     // 1. Decrement amount from the sender.
-//     const getPerson = await prisma.person.findUnique({
-//       where: {
-//         mail: from,
-//       }
-//     })
-
-//     const getAccount = await prisma.account.findUnique({
-//       where: {
-        
-//       }
-//     })
+const prisma = new PrismaClient()
 
 
+export async function transfer(from: number, to: number, amount: number) {
+    return await prisma.$transaction(async (prisma) => {
+    // 1. Decrement amount from the sender.
+    const getSenderPerson = await prisma.person.findUnique({
+      where: {
+        idPerson: from
+      }
+    })
 
-//     // const sender = await prisma.account.update({
-//     //   data: {
-//     //     balance: {
-//     //       decrement: amount,
-//     //     },
-//     //   },
-//     //   where: {
-//     //     balance: 1
-//     //   },
-//     // })
+    const getSenderAccount = await prisma.account.findFirst({
+      where: {
+        personId: getSenderPerson?.idPerson
+      }
+    })
+    console.log( 'getSenderAccount' + JSON.stringify(getSenderAccount))
+
+    const sender = await prisma.account.update({
+      data: {
+        balance: {
+          decrement: amount,
+        },
+      },
+      where: {
+        idAccount: getSenderAccount?.idAccount
+      },
+    })
+    console.log( 'sender' + JSON.stringify(sender))
     
-//     // 2. Verify that the sender's balance didn't go below zero.
-//     // if (sender.balance < 0) {
-//     //   throw new Error(`${from} doesn't have enough to send ${amount}`)
-//     // }
+    // 2. Verify that the sender's balance didn't go below zero.
+    if (sender.balance < 0) {
+      throw new Error(`${from} doesn't have enough to send ${amount}`)
+    }
    
-//     // 3. Increment the recipient's balance by amount
-//     // const recipient = prisma.account.update({
-//     //   data: {
-//     //     balance: {
-//     //       increment: amount,
-//     //     },
-//     //   },
-//     //   where: {
-//     //     mail: to,
-//     //   },
-//     // })
-//     // return recipient
-//   })
-// }
+    // 3. Increment the recipient's balance by amount
+    const getRecipientPerson = await prisma.person.findUnique({
+      where: {
+        idPerson: to
+      }
+    })
+    console.log( 'getRecipientPerson' + JSON.stringify(getRecipientPerson))
+
+    const getRecipientAccount = await prisma.account.findFirst({
+      where: {
+        personId: getRecipientPerson?.idPerson
+      }
+    })
+    console.log( 'getRecipientAccount' + JSON.stringify(getRecipientAccount))
+
+    const recipient = prisma.account.update({
+      data: {
+        balance: {
+          increment: amount,
+        },
+      },
+      where: {
+        idAccount: getRecipientAccount?.idAccount
+      },
+    })
+    console.log('recipient' + JSON.stringify(recipient))
+    return recipient
+  })
+}
 
 
 // async function main() {
 //   // This transfer is successful
-//   await transfer('dp@gmail.com', 'xp@gmail.com', 100)
+//   await transfer(4, 3, 100)
 // }
 
 
--- back-end/src/routes/root.ts
+++ back-end/src/routes/root.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsync } from 'fastify'
 import { PrismaClient } from '@prisma/client'
+import { transfer } from '../controllers/transfer'
 
 const prisma = new PrismaClient()
 
@@ -10,7 +11,9 @@ interface GetClient {
 interface GetAmount {
    amount_withdrawn: Number,
    amount_depose: Number,
-   amount_transfer: Number
+   amount_transfer: number,
+   person_id_origin: number,
+   person_to_transfert: number
 }
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
@@ -18,10 +21,12 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
    fastify.register(require('fastify-formbody'))
 
    fastify.get('/', async (request, reply:any) => {
-      const balance = await prisma.account.findMany()
-      // console.log("..#" + balance)
+      const balance = await prisma.account.findMany({
+         orderBy: { personId: 'asc' }
+      })
       const listPerson = await prisma.person.findMany({
-         where: { roleId: 2 }
+         where: { roleId: 2 },
+         orderBy: { idPerson: 'asc' }
       })
       reply.view('src/views/home', { listPerson: listPerson, balance: balance })
       
@@ -41,17 +46,22 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
    })
 
    fastify.post('/addWithdrawn', async (request, reply:any) => {
-      const {amount_withdrawn} = request.body as GetAmount
+      //const {amount_withdrawn} = request.body as GetAmount
       return request.body
    })
 
    fastify.post('/addDepose', async (request, reply:any) => {
-      const {amount_depose} = request.body as GetAmount
+      //const {amount_depose} = request.body as GetAmount
       return request.body
    })
 
    fastify.post('/addTransfer', async (request, reply:any) => {
-      const {amount_transfer} = request.body as GetAmount
+      const {amount_transfer, person_id_origin, person_to_transfert} = request.body as GetAmount
+      console.log( 'amount_transfer : ' + +amount_transfer, 'person_id_origin : ' +  +person_id_origin, 'person_to_transfert : ' + +person_to_transfert)
+      console.log(JSON.stringify(request.body))
+      if (amount_transfer && person_id_origin && person_to_transfert) {
+         return transfer(+person_id_origin, +person_to_transfert, +amount_transfer)
+      }
       return request.body
    })
 }
